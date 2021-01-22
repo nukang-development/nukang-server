174ded753a7bb6d0d7bd27d1d8c7f33cbb3b068b
adhim1st <adhim@student.uns.ac.id>
2021-01-22 18:30:13 +0700

    Add model and controller for tukang - Add get one tukang

--- services/nukang/routes/tukang.js
+++ services/nukang/routes/tukang.js
@@ -3,4 +3,5 @@ const TukangController = require("../controllers/tukang-controller");
 
 router.put("/:id", TukangController.updateTukang);
 router.post("/login", TukangController.loginTukang);
+router.get("/:id", TukangController.findOneTukang);
 module.exports = router;
