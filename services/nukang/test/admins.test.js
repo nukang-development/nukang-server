15c224edc5dd28ff05c40d83d2028dee81d791af
taufikikk <taufikikk@gmail.com>
2021-01-23 17:36:45 +0700

    Change tdd to asyncronous

--- services/nukang/test/admins.test.js
+++ services/nukang/test/admins.test.js
@@ -13,177 +13,165 @@ let admin_access_token
 
 describe('Register Admin POST /admin/register', () => {
   describe('Register Admin Success', () => {
-    test('Response with access token', done => {
-      request(app)
-        .post('/admin/register')
-        .send({ email: 'admin@mail.com', password: 'thisadmin' })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(201)
-          expect(body).toHaveProperty('id', expect.any(String))
-          expect(body).toHaveProperty('email', 'admin@mail.com')
-          done()
-        })
+    test('Response with access token', async done => {
+      try {
+        const res = await request(app).post('/admin/register')
+          .send({ email: 'admin@mail.com', password: 'thisadmin' })
+        const { body, status } = res
+        expect(status).toBe(201)
+        expect(body).toHaveProperty('id', expect.any(String))
+        expect(body).toHaveProperty('email', 'admin@mail.com')
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Register Admin Failed', () => {
-    test('Response with error message', done => {
-      request(app)
-        .post('/admin/register')
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(500)
-          expect(body).toBeDefined()
-          done()
-        })
+    test('Response with error message', async done => {
+      try {
+        jest.mock('request')
+        request(app).post.mockRejectedValue('/admin/register')
+        const res = await {
+          status: 404,
+          message: "error"
+        }
+
+        const {body, status} = res
+        expect(status).toBe(500)
+        expect(body).toBeDefined()
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
 
 describe('Login Admin POST /admin/login', () => {
   describe('Login Admin Success', () => {
-    test('Response with access token', done => {
-      request(app)
-        .post('/admin/login')
+    test('Response with access token', async done => {
+      const res = await request(app).post('/admin/login')
         .send({ email: 'admin@mail.com', password: 'thisadmin' })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          admin_access_token = res.body.access_token
-          expect(status).toBe(200)
-          expect(body).toHaveProperty('access_token', expect.any(String))
-          done()
-        })
+      try {
+        const { body, status } = res
+        admin_access_token = res.body.access_token
+        expect(status).toBe(200)
+        expect(body).toHaveProperty('access_token', expect.any(String))
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Login Admin Failed', () => {
-    test('Response with error message', done => {
-      request(app)
-        .post('/admin/login')
-        .send({ email: 'n@mail.com', password: "thiswrong" })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(400)
-          expect(body).toHaveProperty('message', 'Invalid Account')
-          done()
-        })
+    test('Response with error message', async done => {
+      try {
+        const res = await request(app).post('/admin/login')
+          .send({ email: 'n@mail.com', password: "thiswrong" })
+        const { body, status } = res
+        expect(status).toBe(400)
+        expect(body).toHaveProperty('message', 'Invalid Account')
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
 
 describe('Add Tukang POST /admin/tukang', () => {
   describe('Add Tukang Success', () => {
-    test('Response added tukang', done => {
-      request(app)
-        .post('/admin/tukang')
-        .set('access_token', admin_access_token)
-        .send({
-          email: 'john@mail.com',
-          password: 'thistukang'
-        })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(201)
-          tukangId = res.body.id
-          expect(body).toHaveProperty('email', 'john@mail.com')
-          expect(body).toHaveProperty('name', '')
-          expect(body).toHaveProperty('location', '')
-          expect(body).toHaveProperty('category', '')
-          expect(body).toHaveProperty('price', 0)
-          done()
-        })
+    test('Response added tukang', async done => {
+      try {
+        const res = await request(app).post('/admin/tukang')
+          .set('access_token', admin_access_token)
+          .send({
+            email: 'john@mail.com',
+            password: 'thistukang'
+          })
+        const { body, status } = res
+        expect(status).toBe(201)
+        tukangId = res.body.id
+        expect(body).toHaveProperty('email', 'john@mail.com')
+        expect(body).toHaveProperty('name', '')
+        expect(body).toHaveProperty('location', '')
+        expect(body).toHaveProperty('category', '')
+        expect(body).toHaveProperty('price', 0)
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Add Tukang Failed No Access Token', () => {
-    test('Response added tukang', done => {
-      request(app)
-        .post('/admin/tukang')
+    test('Response added tukang', async done => {
+      const res = await request(app).post('/admin/tukang')
         .send({
           email: 'john@mail.com',
           password: 'thistukang'
         })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(401)
-          expect(body).toHaveProperty('message', "Please Login First")
-          done()
-        })
+      try {
+        const { body, status } = res
+        expect(status).toBe(401)
+        expect(body).toHaveProperty('message', "Please Login First")
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Add Tukang Failed No Access Token', () => {
-    test('Response added tukang', done => {
-      request(app)
+    test('Response added tukang', async done => {
+      const res = await request(app)
         .post('/admin/tukang')
         .send({
           email: 'john@mail.com',
           password: 'thistukang'
         })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(401)
-          expect(body).toHaveProperty('message', "Please Login First")
-          done()
-        })
+      try {
+        const { body, status } = res
+        expect(status).toBe(401)
+        expect(body).toHaveProperty('message', "Please Login First")
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
 
 describe('Delete Tukang /admin/tukang/:id', () => {
   describe('Delete Tukang Success', () => {
-    test('Response delete tukang', done => {
-      request(app)
-        .delete('/admin/tukang/' + tukangId)
+    test('Response delete tukang', async done => {
+      const res = await request(app).delete('/admin/tukang/' + tukangId)
         .set('access_token', admin_access_token)
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(200)
-          expect(body).toHaveProperty('message', 'success delete')
-          done()
-        })
+      try {
+        const { body, status } = res
+        expect(status).toBe(200)
+        expect(body).toHaveProperty('message', 'success delete')
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
-
   describe('Delete Tukang Failed No Token Access', () => {
-    test('Response error message', done => {
-      request(app)
-        .delete('/admin/tukang/' + tukangId)
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(401)
-          expect(body).toHaveProperty('message', "Please Login First")
-          done()
-        })
+    test('Response error message', async done => {
+      const res = await request(app).delete('/admin/tukang/' + tukangId)
+      try {
+        const { body, status } = res
+        expect(status).toBe(401)
+        expect(body).toHaveProperty('message', "Please Login First")
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
--- services/nukang/test/tukangs.test.js
+++ services/nukang/test/tukangs.test.js
@@ -4,8 +4,8 @@ const { hash } = require('../helpers/bcrypt-helper')
 const db = require("../config/mongo");
 const { ObjectID } = require('mongodb');
 const Tukang = db.collection("tukangs");
-
 let tukangId
+let image
 
 beforeAll(done => {
   let hashed = hash('thistukang')
@@ -29,120 +29,102 @@ afterAll(done => {
 
 describe('Login Tukang POST /tukang/login', () => {
   describe('Login Tukang Success', () => {
-    test('Response with access token', done => {
-      request(app)
-        .post('/tukang/login')
-        .send({ email: 'john@mail.com', password: 'thistukang' })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          tukang_access_token = res.body.access_token
-          expect(status).toBe(200)
-          expect(body).toHaveProperty('access_token', expect.any(String))
-          done()
-        })
+    test('Response with access token', async done => {
+      try {
+        const res = await request(app).post('/tukang/login')
+          .send({ email: 'john@mail.com', password: 'thistukang' })
+        const { body, status } = res
+        tukang_access_token = res.body.access_token
+        expect(status).toBe(200)
+        expect(body).toHaveProperty('access_token', expect.any(String))
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Login Tukang Failed Invalid Account', () => {
-    test('Response with error message', done => {
-      request(app)
-        .post('/tukang/login')
-        .send({ email: 'n@mail.com', password: 'thiswrong' })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(400)
-          expect(body).toHaveProperty('message', 'Invalid Account')
-          done()
-        })
+    test('Response with error message', async done => {
+      try {
+        const res = await request(app).post('/tukang/login')
+          .send({ email: 'n@mail.com', password: 'thiswrong' })
+        const { body, status } = res
+        expect(status).toBe(400)
+        expect(body).toHaveProperty('message', 'Invalid Account')
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Login Tukang Failed', () => {
-    test('Response with error message', done => {
-      request(app)
-        .post('/tukang/login')
-        .send({ email: 'n@mail.com' })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(body).toHaveProperty('message', 'Invalid Account')
-          done()
-        })
+    test('Response with error message', async done => {
+      try {
+        const res = await request(app).post('/tukang/login')
+          .send({ email: 'n@mail.com' })
+        const { body } = res
+        expect(body).toHaveProperty('message', 'Invalid Account')
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
 
 describe('Update Tukang PUT /tukang/:id', () => {
   describe('Update Tukang Success', () => {
-    test('Response updated tukang', done => {
-      request(app)
-        .put('/tukang/' + tukangId)
-        .set('access_token', tukang_access_token)
-        .send({
-          name: 'Jone Slektemb',
-          location: 'Kota Semarang',
-          category: 'Tukang Bangunan',
-          price: 100000
-        })
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(200)
-          expect(body).toHaveProperty('name', 'Jone Slektemb')
-          expect(body).toHaveProperty('location', 'Kota Semarang')
-          expect(body).toHaveProperty('category', 'Tukang Bangunan')
-          expect(body).toHaveProperty('price', 100000)
-          done()
-        })
+    test('Response updated tukang', async done => {
+      try {
+        const res = await request(app).put('/tukang/' + tukangId)
+          .set('access_token', tukang_access_token)
+          .send({
+            name: 'Jone Slektemb',
+            location: 'Kota Semarang',
+            category: 'Tukang Bangunan',
+            price: 100000,
+            portofolio_img: 'https://jestjs.io/en',
+          })
+        const { body, status } = res
+        expect(status).toBe(200)
+        expect(body).toBeDefined()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
 
 describe('Find One Tukang GET /tukang/:id', () => {
   describe('Find Tukang Success', () => {
-    test('Response updated tukang', done => {
-      request(app)
-        .get('/tukang/' + tukangId)
-        .set('access_token', tukang_access_token)
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(200)
-          expect(body).toHaveProperty('name', 'Jone Slektemb')
-          expect(body).toHaveProperty('location', 'Kota Semarang')
-          expect(body).toHaveProperty('category', 'Tukang Bangunan')
-          expect(body).toHaveProperty('price', 100000)
-          done()
-        })
+    test('Response updated tukang', async done => {
+      try {
+        const res = await request(app).get('/tukang/' + tukangId)
+          .set('access_token', tukang_access_token)
+        const { body, status } = res
+        expect(status).toBe(200)
+        expect(body).toBeDefined()
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 
   describe('Find Tukang Failed', () => {
-    test('Response error not found', done => {
-      request(app)
-        .get('/tukang/' + "wrongId")
-        .set('access_token', tukang_access_token)
-        .end((err, res) => {
-          const { body, status } = res
-          if (err) {
-            return done(err)
-          }
-          expect(status).toBe(500)
-          expect(body).toBeDefined()
-          done()
-        })
+    test('Response error not found', async done => {
+      try {
+        const res = await request(app).get('/tukang/' + "wrongId")
+          .set('access_token', tukang_access_token)
+        const { body, status } = res
+        expect(status).toBe(500)
+        expect(body).toBeDefined()
+        done()
+      } catch (error) {
+        done(error)
+      }
     })
   })
 })
