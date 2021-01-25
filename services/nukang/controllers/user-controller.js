9c37bc1340db75ec869138a1645fa1508f1fb21c
adhim1st <adhim@student.uns.ac.id>
2021-01-25 09:00:33 +0700

    Add getTukangProfile routing in user routes

--- services/nukang/controllers/user-controller.js
+++ services/nukang/controllers/user-controller.js
@@ -10,17 +10,17 @@ class UserController {
       if (req.body.email === "") {
         throw {
           status: 400,
-          message: "Please Fill Email"
-        }
+          message: "Please Fill Email",
+        };
       } else {
         const data = await UserModel.register({
           email: req.body.email,
           password: req.body.password,
-        })
+        });
         res.status(201).json({ id: data._id, email: data.email });
       }
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
@@ -54,56 +54,33 @@ class UserController {
           contact: req.body.contact,
           address: req.body.address,
           total_price: req.body.total_price,
-        })
+        });
         res.status(201).json(data);
       } else {
         throw {
           status: 400,
-          message: "User ID required"
-        }
+          message: "User ID required",
+        };
       }
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async findByUser(req, res, next) {
     try {
       if (!Number(req.params.id)) {
-        const data = await OrderModel.findAllbyUser(req.params.id)
+        const data = await OrderModel.findAllbyUser(req.params.id);
         res.status(200).json(data);
       } else {
         throw {
           status: 404,
-          message: "Error Not Found"
-        }
+          message: "Error Not Found",
+        };
       }
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
-
-  // get detail profile tukang
-  static getTukangDetail(req, res, next) {
-    TukangModel.findOne(req.params.id)
-      .then((data) => {
-        res.status(200).json({
-          id: data._id,
-          name: data.name,
-          location: data.location,
-          category: data.category,
-          small_project_desc: data.small_project_desc,
-          small_project_price: data.small_project_price,
-          medium_project_desc: data.medium_project_desc,
-          medim_project_price: data.medium_project_price,
-          big_project_desc: data.big_project_desc,
-          big_project_price: data.big_project_price,
-          portofolio_img: data.portofolio_img,
-        });
-      })
-      .catch((err) => {
-        next(err);
-      });
   }
 }
 
